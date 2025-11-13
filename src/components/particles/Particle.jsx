aef24d1eef7513008b5a777d550133f931cd2e41
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-11-13 22:40:41 +0100

    small aria fix

--- src/components/particles/Particle.jsx
+++ src/components/particles/Particle.jsx
@@ -4,7 +4,7 @@ export default function Particle({y}) {
 
     const animationTime = Math.random() * 3 + 1;
     return (
-        <div className={`fixed bg-white w-[10px] h-[10px] particle-animation z-1`} style={{
+        <div className={`fixed bg-white w-[10px] h-[10px] particle-animation z-1`} aria-hidden={true} style={{
             top:`${y}px`,
             animationDuration:`${animationTime}s`
         }}>
